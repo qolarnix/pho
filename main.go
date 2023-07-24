854e58341884fb350e6258a3ddca3ec26c8dc818
polarnix <polarnix@pm.me>
2023-07-23 17:18:38 -0700

    testing

--- main.go
+++ main.go
@@ -3,20 +3,13 @@ package main
 import (
 	"embed"
 	"fmt"
-	"io/fs"
 	"log"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
-	"io"
+
 	// "net/http"
 
 	"github.com/webview/webview"
 )
-
-//go:embed bin/php
-var php []byte
 
 //go:embed app/*
 var app embed.FS
@@ -31,53 +24,7 @@ func main() {
 	// fmt.Println("go listening: 3030")
 	// http.ListenAndServe(":3030", nil)
 
-	mountDir, err := os.MkdirTemp("", "app")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.RemoveAll(mountDir)
-
-	binPath := filepath.Join(mountDir, "php")
-	err = os.WriteFile(binPath, php, 0700)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = fs.WalkDir(app, "app", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		file, err := app.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		stripPrefix := strings.TrimPrefix(path, "app/")
-		fmt.Println(stripPrefix)
-
-		writeFile, err := os.Create(filepath.Join(mountDir, stripPrefix))
-		if err != nil {
-			return err
-		}
-		defer writeFile.Close()
-
-		_, err = io.Copy(writeFile, file)
-		return err
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(binPath)
-	fmt.Println(mountDir)
-
-	serve := exec.Command(binPath, "-S", "localhost:3000", "-t", mountDir)
+	serve := exec.Command("app/php", "-S", "localhost:3000", "-t", "app")
 	err = serve.Start()
 	if err != nil {
 		log.Fatal(err)
